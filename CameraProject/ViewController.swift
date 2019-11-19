95ec6c9274eb1986342809700f9737af5cf32de6
SUB_ACCOUNT <SUB_ADDRESS>
2019-11-19 14:05:13 +0800

    first commit

---
+++ CameraProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraProject
+//
+//  Created by shunya endoh on 2019/11/19.
+//  Copyright © 2019 shunya endoh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
